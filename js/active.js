1c4867b23f5060277e757461b9d6c64b59782ed8
Jonathan <jhongama_jesus@hotmail.com>
2019-07-24 10:49:42 -0400

    preload ok

--- js/active.js
+++ js/active.js
@@ -4,11 +4,10 @@
     var browserWindow = $(window);
 
     // :: 1.0 Preloader Active Code
-    browserWindow.on('load', function () {
-        $('#preloader').fadeOut('slow', function () {
-            $(this).remove();
-        });
-    });
+    $(document).ready(function(){
+          setTimeout('$("#preload").fadeOut(100)', 1750);
+       
+      });
 
     // :: 2.0 Nav Active Code
     if ($.fn.classyNav) {
