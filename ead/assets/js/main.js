4b6366c9a89f790a56ba00bbd8446873b705c2a9
Jonathan <jhongama_jesus@hotmail.com>
2019-07-29 11:34:00 -0400

    Page curso{id} ok

--- ead/assets/js/main.js
+++ ead/assets/js/main.js
@@ -98,3 +98,24 @@
     }); 	   
  
 })(jQuery); 
+
+var starttime = 2;
+$(function() {
+    $("#playlist li").on("click", function() {
+        $("#videoarea").attr({
+            "src": $(this).attr("movieurl"),
+            "poster": "",
+            "autoplay": "autoplay"
+                  })
+      starttime = $(this).attr('startt');
+    })
+//     $("#videoarea").attr({
+//         "src": $("#playlist li").eq(0).attr("movieurl"),
+//         "poster": $("#playlist li").eq(0).attr("moviesposter"),
+   
+//     })
+})
+
+document.getElementById("videoarea").addEventListener("loadedmetadata", function() {
+     this.currentTime = starttime;
+}, false);
