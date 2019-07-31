c2bd8099630125c45032115ab001c583dab5acf6
Jonathan <jhongama_jesus@hotmail.com>
2019-07-31 11:45:05 -0400

    Site pronto

--- js/active.js
+++ js/active.js
@@ -140,3 +140,10 @@ $('.owl-carousel').owlCarousel({
     }
 })
 
+ var $doc = $('html, body');
+$('a').click(function() {
+    $doc.animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+    return false;
+});
